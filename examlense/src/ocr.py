7528bb5371024fa7493f54b492a54e009814a584
Aditya prasad Barik <codesheritage@gmail.com>
2026-03-27 13:20:21 +0530

    switched to larger transformer for better performance...

--- examlense/src/ocr.py
+++ examlense/src/ocr.py
@@ -3,8 +3,8 @@ from PIL import Image
 # import requests if we will take images from the web
 
 def load_model():
-    processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
-    model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
+    processor = TrOCRProcessor.from_pretrained('microsoft/trocr-large-handwritten')
+    model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-large-handwritten')
     return processor, model
 
 #image=Image.open(requests.get('https://raw.githubusercontent.com/huggingface/notebooks/main/examples/datasets/ocr/handwritten.png', stream=True).raw).convert('RGB')
@@ -12,12 +12,12 @@ def load_model():
 def extract_text(img_path,processor, model):
     image=Image.open(img_path).convert('RGB')
     pixel_values=processor(images=image,return_tensors='pt').pixel_values
-    generated_ids=model.generate(pixel_values)
+    generated_ids=model.generate(pixel_values,max_new_tokens=512)
     text=processor.batch_decode(generated_ids,skip_special_tokens=True)[0]
     return text
 
 #for testing purposes
 if __name__== '__main__':
     processor, model = load_model()
-    text = extract_text(r'C:\Users\codes\Desktop\Exam lense\examlense\uploads\test.jpeg', processor, model)
+    text = extract_text(r'C:\Users\codes\Desktop\Exam lense\examlense\uploads\preprocessed_test2.jpeg', processor, model)
     print(text)
