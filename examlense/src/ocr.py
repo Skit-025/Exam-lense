79c831eb81f2dd968c44a34bc2a095fa6f2c0ade
Aditya prasad Barik <codesheritage@gmail.com>
2026-03-27 13:46:56 +0530

    Update ocr.py

--- examlense/src/ocr.py
+++ examlense/src/ocr.py
@@ -12,7 +12,7 @@ def load_model():
 def extract_text(img_path,processor, model):
     image=Image.open(img_path).convert('RGB')
     pixel_values=processor(images=image,return_tensors='pt').pixel_values
-    generated_ids=model.generate(pixel_values,max_new_tokens=512)
+    generated_ids=model.generate(pixel_values,max_new_tokens=51)
     text=processor.batch_decode(generated_ids,skip_special_tokens=True)[0]
     return text
 
