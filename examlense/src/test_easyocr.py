afeb04181ec0b7c7a27f4c9bd1e16b84b5cdd0f2
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-01 02:01:41 +0530

    Update test_easyocr.py

--- examlense/src/test_easyocr.py
+++ examlense/src/test_easyocr.py
@@ -1,4 +1,4 @@
 import easyocr
 reader = easyocr.Reader(['en'])
-result = reader.readtext(r'C:\Users\codes\Desktop\Exam lense\examlense\uploads\.jpeg', detail=0)
+result = reader.readtext(r'C:\Users\codes\Desktop\Exam lense\examlense\uploads\CroppedLines\test\line_test_1.jpeg', detail=0)
 print(' '.join(result))
